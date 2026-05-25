aacc20d1fd849bbc1d89bf17dd72b1ff0c27fdb2
johnlatif16 <johnlatif37@gmail.com>
2026-05-25 21:55:54 +0300

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -2,12 +2,15 @@ self.addEventListener('push', function(event) {
     const data = event.data.json();
     self.registration.showNotification(data.title, {
         body: data.body,
-        icon: '/icon.png',
+        icon: '/favicon.ico',
+        badge: '/favicon.ico',
         data: { url: data.url || '/' }
     });
 });
 
 self.addEventListener('notificationclick', function(event) {
     event.notification.close();
-    event.waitUntil(clients.openWindow(event.notification.data.url));
+    event.waitUntil(
+        clients.openWindow(event.notification.data.url)
+    );
 });
